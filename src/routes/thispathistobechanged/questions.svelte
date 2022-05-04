313074dd9e0b6d124f9d1d12edae1c246cb98b3c
Finn Wählt <social.finn.waehlt@icloud.com>
2022-05-04 11:53:33 +0200

    Commit 1

--- src/routes/thispathistobechanged/questions.svelte
+++ src/routes/thispathistobechanged/questions.svelte
@@ -1,26 +1,45 @@
 <script lang="ts">
 import { onMount } from "svelte";
 
-let elapsedTime: number;
+let elapsedTime: number = 0; // global?
 
 let Questions : string = "Question to be asked"
 let Answer : string = "Answer to be defined here" //maybe have nultiple answers if there is a bit of ambiguity
 let Hints : string[] = ["Hint 1", "Hint 2"]
 
-
 // todo define a timer that starts on page first visited and shows hints
-// 
 
 onMount(async () => {
  //2 mins elapsed
+ //check if intervalId exists or else
+    if(elapsedTime == 0){
+        setInterval(changeElapsedTime, 4000) //change to 120.000 for 2 minutes
+    }
 });
+
+function changeElapsedTime(){
+    elapsedTime += 1;
+}
+
 </script>
 
 <main>
 Here go the to be defined questions with help.
 A reference to morsecode route is needed here
+
+{#if elapsedTime >= 1}
+    <p class="hint">HintOne</p>
+{/if}
+{#if elapsedTime >= 2}
+    <p class="hint">HintTwo</p>
+{/if}
+
+
+
 </main>
 
 <style>
-
+    .hint{
+        font-size: large;
+    }
 </style>
