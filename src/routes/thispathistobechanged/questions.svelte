ce3578c51dabf5e7b551a7b0f65b1d3c8c64ce7b
Finn Wählt <social.finn.waehlt@icloud.com>
2022-05-04 15:55:51 +0200

    added form

--- src/routes/thispathistobechanged/questions.svelte
+++ src/routes/thispathistobechanged/questions.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 
 import { onDestroy, onMount } from "svelte";
+import { writable } from "svelte/store";
 
-let elapsedTime: number = 0; // global?
+let elapsedTime : number = 0; // global?
 
 let Questions : string = "Question to be asked"
-let Answer : string = "Answer to be defined here" //maybe have nultiple answers if there is a bit of ambiguity
+let Answer : string = "123" //maybe have multiple answers if there is a bit of ambiguity
 let Hints : string[] = ["Hint 1", "Hint 2"]
 
 // todo define a timer that starts on page first visited and shows hints
@@ -13,7 +14,7 @@ let Hints : string[] = ["Hint 1", "Hint 2"]
 onMount(async () => 
 {
     if(elapsedTime == 0){
-        setInterval(changeElapsedTime, 4000) //change to 120.000 for 2 minutes
+        setInterval(changeElapsedTime, 1000) //change to 120.000 for 2 minutes
     }
 });
 
@@ -22,21 +23,40 @@ function changeElapsedTime(){
     elapsedTime += 1;
 }
 
+   //FormValidation
+function validateForm() {
+  let x = document.forms["answerform"]["fanswer"].value;
+  if (x == Answer) { //Referenz Auf die AnwortVariable
+    return true
+  }
+}
+
+
+
 </script>
 
 <main>
     
 Here go the to be defined questions with help.
-A reference to morsecode route is needed here
+A reference to morsecode route is needed here.
 
-{#if elapsedTime >= 1}
-    <p class="hint">HintOne</p>
+<p class="question">{Questions}</p>
+
+{#if elapsedTime >= 0}
+    <p class="hint">{Hints[0]}</p>
 {/if}
-{#if elapsedTime >= 2}
-    <p class="hint">HintTwo</p>
+{#if elapsedTime >= 0}
+    <p class="hint">{Hints[1]}</p>
 {/if}
 
+<form name="answerform" onsubmit="return validateForm()" action="Redirect" method="post">
+    Answer: <input type="text" name="fanswer">
+    <input type="submit" value="Submit">
+</form>
 
+<script>
+ 
+</script>
 
 </main>
 
