c53e3a3858505bed68cbfbf23aadd5d5764d4799
raphaellutz <raphael.lutz.development@gmail.com>
2022-05-04 09:50:40 +0200

    added onMount method in questions.svelte

--- src/routes/thispathistobechanged/questions.svelte
+++ src/routes/thispathistobechanged/questions.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+import { onMount } from "svelte";
+
+let elapsedTime: number;
 
 let Questions : string = "Question to be asked"
 let Answer : string = "Answer to be defined here" //maybe have nultiple answers if there is a bit of ambiguity
@@ -7,6 +10,10 @@ let Hints : string[] = ["Hint 1", "Hint 2"]
 
 // todo define a timer that starts on page first visited and shows hints
 // 
+
+onMount(async () => {
+ //2 mins elapsed
+});
 </script>
 
 <main>
