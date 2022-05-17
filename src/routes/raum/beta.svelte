052f508e33ccac78657277479f2558499569bcb9
raphaellutz <raphael.lutz.development@gmail.com>
2022-05-17 15:11:33 +0200

    Added beta

---
+++ src/routes/raum/beta.svelte
@@ -0,0 +1,17 @@
+<script lang=ts>
+
+    //javascript goes here
+    
+</script>
+    
+<main>
+    
+    <img src="../Morse/Beta_Morsetabelle.png" alt="Bild von Morsecode" width="200px">
+    <p></p>
+    <a sveltekit:prefetch href="/deineinzigerausweg/frage">Question page</a>
+    
+</main>
+    
+<style>
+    
+</style>
