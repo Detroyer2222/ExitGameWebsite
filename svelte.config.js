99d56fcaf3a652308b5a5c39dece3abd9d3bb394
Detroyer2222 <49248383+Detroyer2222@users.noreply.github.com>
2022-05-18 00:49:07 +0200

    Fixed error in frage seite

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,13 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(
+			{
+				pages: 'build',
+				assets: 'build',
+				fallback: null
+			}
+		)
 	}
 };
 
